82023aa05659686da643bfe226ea8bf1fee186b7
RoboT-Dave <pawwwll@gmail.com>
2021-09-17 14:13:10 +0200

    Remove old code

--- webapp.py
+++ webapp.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from threading import Thread
 from os import environ
 
 app = Flask( __name__ )
@@ -8,8 +7,4 @@ app = Flask( __name__ )
 def home():
     return "cock and balls"
 
-#def run():
 app.run( host = '0.0.0.0', port = environ["PORT"] )
-
-#t = Thread( target = run )
-#t.start()
