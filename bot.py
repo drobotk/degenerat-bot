8e5677e5bfba14b1dc296e4c2bbf84cf826bcd6e
drobotk <pawwwll@gmail.com>
2022-03-15 22:03:53 +0100

    Add /ttt

--- bot.py
+++ bot.py
@@ -15,20 +15,7 @@ bot = Bot(command_prefix=",", help_command=None, intents=intents)
 # go ahead and hate me for this
 async def sync_globals_to(guild: discord.Guild):
     _log.info(f'Syncing commands to guild "{guild}"')
-
-    if guild.id in bot.tree._guild_commands:
-        bot.tree._guild_commands[guild.id].update(bot.tree._global_commands)
-    else:
-        bot.tree._guild_commands[guild.id] = bot.tree._global_commands
-
-    context_cmds = {}
-    for info, cmd in bot.tree._context_menus.items():
-        if info[1] is None:
-            new_info = (info[0], guild.id, info[2])
-            context_cmds[new_info] = cmd
-
-    bot.tree._context_menus.update(context_cmds)
-
+    bot.tree.copy_global_to(guild=guild)
     await bot.tree.sync(guild=guild)
 
 
@@ -52,21 +39,21 @@ async def on_guild_join(guild: discord.Guild):
 
 
 async def runner():
-    async with bot:
-        await bot.load_extension("jishaku")
-        await bot.load_extension("modules.stoi")
-        await bot.load_extension("modules.activities")
-        await bot.load_extension("modules.figlet")
-        await bot.load_extension("modules.cow")
-        await bot.load_extension("modules.react")
-        await bot.load_extension("modules.img")
-        await bot.load_extension("modules.ryj")
-        await bot.load_extension("modules.music.cog")
-        await bot.load_extension("modules.status")
-        await bot.load_extension("modules.triggers")
-        await bot.load_extension("modules.info")
+    await bot.load_extension("jishaku")
+    await bot.load_extension("modules.stoi")
+    await bot.load_extension("modules.activities")
+    await bot.load_extension("modules.figlet")
+    await bot.load_extension("modules.cow")
+    await bot.load_extension("modules.react")
+    await bot.load_extension("modules.img")
+    await bot.load_extension("modules.ryj")
+    await bot.load_extension("modules.music.cog")
+    await bot.load_extension("modules.status")
+    await bot.load_extension("modules.triggers")
+    await bot.load_extension("modules.info")
+    await bot.load_extension("modules.ttt")
 
-        await bot.start(os.getenv("DISCORD_TOKEN"))
+    await bot.start(os.getenv("DISCORD_TOKEN"))
 
 
 def main():
@@ -77,7 +64,6 @@ def main():
 
     try:
         asyncio.run(runner())
-
     except KeyboardInterrupt:
         return
 
---
+++ modules/ttt.py
@@ -0,0 +1,143 @@
+from email import header
+from enum import Enum, auto
+import discord
+from discord.ext import commands
+from discord import app_commands, ui
+
+
+class TTTState(Enum):
+    Gameplay = auto()
+    XWon = auto()
+    OWon = auto
+    Tie = auto()
+
+
+class TTTGameView(ui.View):
+    header: str = "**--- Kółko i Krzyżyk ---**\n"
+    gameplay: str = "Ruch: {}"
+    win: str = "Koniec gry! Wygrywa: {}"
+    tie: str = "Koniec gry - remis!"
+    X: str = "❌"
+    O: str = "⭕"
+
+    win_states: list[tuple[int]] = [
+        # horizontal
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),
+        # vertical
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),
+        # diagonal
+        (0, 4, 8), (2, 4, 6),  
+    ]
+
+    def __init__(self, player1: discord.User, player2: discord.User):
+        super().__init__(timeout=120)
+        for i in range(9):
+            self.add_item(
+                TTTButton(style=discord.ButtonStyle.blurple, label="▪️", row=int(i / 3))
+            )
+
+        self.players = [player1, player2]
+        self.moves = 0
+        self.state: TTTState = TTTState.Gameplay
+
+    @property
+    def current_move(self) -> discord.User:
+        return self.players[self.moves % 2]
+
+    @classmethod
+    async def send_game(
+        cls,
+        interaction: discord.Interaction,
+        player1: discord.User,
+        player2: discord.User,
+    ):
+        game = cls(player1, player2)
+        text = cls.header + cls.gameplay.format(player1.mention)
+        await interaction.response.send_message(text, view=game)
+
+    def update_state(self) -> TTTState:
+        for indices in self.win_states:
+            btns: list[TTTButton] = [self.children[i] for i in indices]
+            if all([b.label == self.X for b in btns]):
+                self.state = TTTState.XWon
+                break
+            elif all([b.label == self.O for b in btns]):
+                self.state = TTTState.OWon
+                break
+
+        if self.state is not TTTState.Gameplay:
+            for b in self.children:
+                b.disabled = True
+                b.style = discord.ButtonStyle.gray
+
+            for b in btns:
+                b.style = discord.ButtonStyle.green
+
+        elif all([b.disabled for b in self.children]):
+            self.state = TTTState.Tie
+
+        return self.state
+
+
+class TTTButton(ui.Button):
+    view: TTTGameView
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user not in self.view.players:
+            await interaction.response.send_message(
+                f"Nie dla psa! Tutaj grają {self.view.players[0].mention} i {self.view.players[1].mention}. Aby zagrać, użyj komendy /ttt",
+                ephemeral=True,
+            )
+            return
+
+        if self.view.current_move != interaction.user:
+            await interaction.response.send_message(
+                f"Poczekaj na swój ruch!", ephemeral=True
+            )
+            return
+
+        self.disabled = True
+        self.style = discord.ButtonStyle.gray
+        if self.view.moves % 2:
+            self.label = self.view.O
+        else:
+            self.label = self.view.X
+
+        state = self.view.update_state()
+        if state is TTTState.Gameplay:
+            self.view.moves += 1
+            text = self.view.header + self.view.gameplay.format(
+                self.view.current_move.mention
+            )
+
+        elif state is TTTState.Tie:
+            text = self.view.header + self.view.tie
+
+        else:
+            text = self.view.header + self.view.win.format(
+                self.view.current_move.mention
+            )
+
+        await interaction.response.edit_message(content=text, view=self.view)
+
+
+class TicTacToe(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(description="Gra w kółko i krzyżyk")
+    @app_commands.describe(opponent="Twój przeciwnik")
+    async def ttt(self, interaction: discord.Interaction, opponent: discord.User):
+        await TTTGameView.send_game(interaction, interaction.user, opponent)
+
+
+async def setup(bot: commands.Bot):
+    cog = TicTacToe(bot)
+
+    @app_commands.context_menu(name="Zagraj w kółko i krzyżyk")
+    async def ttt_context(interaction: discord.Interaction, opponent: discord.User):
+        await TTTGameView.send_game(interaction, interaction.user, opponent)
+
+    bot.tree.add_command(ttt_context)
+
+    await bot.add_cog(cog)
