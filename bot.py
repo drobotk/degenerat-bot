f54f78828e58b0db70e4028fe2113dd929ebf4f0
drobotk <pawwwll@gmail.com>
2022-03-27 12:09:26 +0200

    Remove automatic command sync

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands
 
 import os
 import asyncio
@@ -10,50 +10,52 @@ _log = logging.getLogger(__name__)
 
 intents = discord.Intents.all()
 intents.presences = False
-bot = Bot(command_prefix=",", help_command=None, intents=intents)
-
-# go ahead and hate me for this
-async def sync_globals_to(guild: discord.Guild):
-    _log.info(f'Syncing commands to guild "{guild}"')
-    bot.tree.copy_global_to(guild=guild)
-    await bot.tree.sync(guild=guild)
+intents.typing = False
+bot = commands.Bot(command_prefix=",", help_command=None, intents=intents)
 
 
 @bot.event
 async def on_ready():
     _log.info(f'Logged in as "{bot.user}"')
+    for g in bot.guilds:
+        bot.tree.copy_global_to(guild=g)
 
-    if not hasattr(bot, "has_synced_commands"):
-        for g in bot.guilds:
-            await sync_globals_to(g)
-            await asyncio.sleep(1)
 
-        bot.has_synced_commands = True
+@bot.command()
+@commands.is_owner()
+async def syncall(ctx: commands.Context):
+    msg = await ctx.send("Syncing all app commands to all guilds...")
+    for n, g in enumerate(bot.guilds, 1):
+        await bot.tree.sync(guild=g)
+        await asyncio.sleep(n)
+
+    await msg.edit("Done")
 
 
 @bot.event
 async def on_guild_join(guild: discord.Guild):
     _log.info(f'Joined guild "{guild}"')
 
-    await sync_globals_to(guild)
+    bot.tree.copy_global_to(guild=guild)
+    await bot.tree.sync(guild=guild)
 
 
 async def runner():
-    await bot.load_extension("jishaku")
-    await bot.load_extension("modules.stoi")
-    await bot.load_extension("modules.activities")
-    await bot.load_extension("modules.figlet")
-    await bot.load_extension("modules.cow")
-    await bot.load_extension("modules.react")
-    await bot.load_extension("modules.img")
-    await bot.load_extension("modules.ryj")
-    await bot.load_extension("modules.music.cog")
-    await bot.load_extension("modules.status")
-    await bot.load_extension("modules.triggers")
-    await bot.load_extension("modules.info")
-    await bot.load_extension("modules.ttt")
-
     async with bot:
+        await bot.load_extension("jishaku")
+        await bot.load_extension("modules.stoi")
+        await bot.load_extension("modules.activities")
+        await bot.load_extension("modules.figlet")
+        await bot.load_extension("modules.cow")
+        await bot.load_extension("modules.react")
+        await bot.load_extension("modules.img")
+        await bot.load_extension("modules.ryj")
+        await bot.load_extension("modules.music.cog")
+        await bot.load_extension("modules.status")
+        await bot.load_extension("modules.triggers")
+        await bot.load_extension("modules.info")
+        await bot.load_extension("modules.ttt")
+
         await bot.start(os.getenv("DISCORD_TOKEN"))
 
 
