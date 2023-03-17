c9d5deda1886730d1aa531b3092629605cc74e80
drobotk <pawwwll@gmail.com>
2023-03-17 23:48:47 +0100

    Add YTPosts cog

---
+++ bot/modules/yt_posts.py
@@ -0,0 +1,139 @@
+from io import BytesIO
+import json
+import logging
+import re
+from typing import Any, Optional
+from PIL import Image, ImageSequence
+
+import discord
+from discord.ext import commands
+
+from ..bot import DegeneratBot
+from ..utils import traverse_obj, dots_after
+
+class YTPosts(commands.Cog):
+    def __init__(self, bot: DegeneratBot):
+        self.bot: DegeneratBot = bot
+        self.log: logging.Logger = logging.getLogger(__name__)
+
+        self.re_link: re.Pattern[str] = re.compile(r"https:\/\/www\.youtube\.com\/post\/[a-zA-Z0-9_\-]{36}")
+        self.re_json: re.Pattern[str] = re.compile(r">var ytInitialData = (\{.+?\});<")
+
+        self.processed_messages: list[int] = []
+
+    async def process_message(self, message: discord.Message):
+        new_embeds: list[discord.Embed] = []
+        files: list[discord.File] = []
+        for e in message.embeds:
+            if not e.url:
+                continue
+
+            if not self.re_link.fullmatch(e.url):
+                continue
+
+            self.log.info(e.url)
+
+            async with self.bot.session.get(e.url, cookies={"SOCS": "CAESNQgDEitib3FfaWRlbnRpdHlmcm9udGVuZHVpc2VydmVyXzIwMjMwMzE0LjA2X3AxGgJwbCACGgYIgNvOoAY", "YSC": "LvvlUwB2Uko", "__Secure-YEC": "Cgt1S1A1WjY1VTZDZyick9OgBg%3D%3D", "CONSENT": "PENDING+757"}) as r:
+                if not r.ok:
+                    self.log.error(f"{e.url} status code {r.status}")
+                    continue
+                
+                text = await r.text()
+            
+            m = self.re_json.search(text)
+            if not m:
+                self.log.error(f"{e.url} didn't match ytInitialData ({r.url=})")
+                continue
+
+            data = json.loads(m.group(1))
+            with open("cock.json", "w") as f:
+                json.dump(data, f)
+
+            post: Optional[dict[str, Any]] = traverse_obj(data, ("contents", "twoColumnBrowseResultsRenderer", "tabs", 0, "tabRenderer", "content", "sectionListRenderer", "contents", 0, "itemSectionRenderer", "contents", 0, "backstagePostThreadRenderer", "post", "backstagePostRenderer"))  # type: ignore (no idea how to type correctly)
+            if not post:
+                self.log.error(f"{e.url} traverse_obj returned None")
+                continue
+
+            embed = discord.Embed(color=message.guild.me.color if message.guild else None)
+            
+            author: Optional[str] = traverse_obj(post, ("authorText", "runs", 0, "text"))  # type: ignore (no idea how to type correctly)
+            if author:
+                thumbnail: Optional[str] = traverse_obj(post, ("authorThumbnail", "thumbnails", 0, "url"))  # type: ignore (no idea how to type correctly)
+                if thumbnail:
+                    thumbnail = "https:" + thumbnail
+
+                urls: Optional[list[str]] = traverse_obj(post, ("authorEndpoint", (("commandMetadata", "webCommandMetadata", "url"), ("browseEndpoint", "canonicalBaseUrl"))))  # type: ignore (no idea how to type correctly)
+                embed.set_author(name=author, icon_url=thumbnail, url=f"https://www.youtube.com{urls[0]}" if urls else None)
+
+            texts: Optional[list[str]] = traverse_obj(post, ("contentText", "runs", ..., "text"))  # type: ignore (no idea how to type correctly)
+            embed.description = dots_after("".join(texts), 4096) if texts else None
+
+            images: Optional[list[dict[str, Any]]] = traverse_obj(post, ("backstageAttachment", "backstageImageRenderer", "image", "thumbnails"))  # type: ignore (no idea how to type correctly)
+            if images:
+                images.sort(key=lambda x: x["width"])
+                url = images[-1]["url"]
+                
+                async with self.bot.session.get(url) as r:
+                    if not r.ok:
+                        self.log.error(f"{e.url} image fetch failed - wtf?")
+                    elif r.content_type != "image/webp":
+                        embed.set_image(url=url)
+                    else:
+                        webp = BytesIO(await r.read())
+                        gif = BytesIO()
+                        with Image.open(webp) as image:
+                            frames: list[Image.Image] = []
+                            for frame in ImageSequence.Iterator(image):        
+                                im2 = Image.new("RGB", frame.size, (255, 255, 255))        
+                                bands = frame.split()
+                                mask = bands[3] if len(bands) > 3 else None
+                                im2.paste(frame, mask=mask)
+                                frames.append(im2.convert('RGB'))
+
+                            frames[0].save(gif,
+                                        format='gif',
+                                        save_all=True,
+                                        append_images=frames[1:],
+                                        optimize=True,
+                                        duration=image.info.get("duration", 10),
+                                        loop=image.info.get("loop", 0),
+                                        quality=100)
+
+                        gif.seek(0)
+                        fn = f'{post["postId"]}.gif'
+                        files.append(discord.File(gif, fn))
+                        embed.set_image(url=f"attachment://{fn}")
+
+            poll: Optional[dict[str, Any]] = traverse_obj(post, ("backstageAttachment", "pollRenderer"))  # type: ignore (no idea how to type correctly)
+            if poll:
+                votes: Optional[str] = traverse_obj(poll, ("totalVotes", "simpleText"))  # type: ignore (no idea how to type correctly)
+                choices: list[str] = traverse_obj(poll, ("choices", ..., "text", "runs", 0, "text")) or []  # type: ignore (no idea how to type correctly)
+                embed.add_field(name=f"Ankieta: {votes}", value="\n".join([f"🔹 {c}" for c in choices]))
+
+            likes: Optional[str] = traverse_obj(post, ("voteCount", "simpleText"))  # type: ignore (no idea how to type correctly)
+            if likes:
+                embed.set_footer(text=f"👍 {likes}")
+
+            new_embeds.append(embed)
+
+        if new_embeds:
+            await message.reply(embeds=new_embeds, files=files, mention_author=False)
+            self.processed_messages.append(message.id)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot or not message.content or not message.embeds:
+            return
+        
+        await self.process_message(message)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, _: discord.Message, after: discord.Message):
+        if after.id in self.processed_messages or after.author.bot or not after.content or not after.embeds:
+            return
+        
+        await self.process_message(after)
+
+
+async def setup(bot: DegeneratBot):
+    await bot.add_cog(YTPosts(bot))
--- bot/utils.py
+++ bot/utils.py
@@ -1,3 +1,8 @@
+import collections, collections.abc
+import contextlib
+import inspect
+import itertools
+import re
 import typing
 from urllib.parse import urlparse
 
@@ -24,3 +29,281 @@ def sub_before(a: str, b: str, c: typing.Optional[str] = None) -> str:
         return c or a
 
     return a[:idx]
+
+##################### yt-dlp/yt-dlp/yt_dlp/utils.py <3
+NO_DEFAULT = object()
+IDENTITY = lambda x: x
+
+def try_call(*funcs, expected_type=None, args=[], kwargs={}):
+    for f in funcs:
+        try:
+            val = f(*args, **kwargs)
+        except (AttributeError, KeyError, TypeError, IndexError, ValueError, ZeroDivisionError):
+            pass
+        else:
+            if expected_type is None or isinstance(val, expected_type):
+                return val
+
+def variadic(x, allowed_types=(str, bytes, dict)):
+    return x if isinstance(x, collections.abc.Iterable) and not isinstance(x, allowed_types) else (x,)
+
+def int_or_none(v, scale=1, default=None, get_attr=None, invscale=1):
+    if get_attr and v is not None:
+        v = getattr(v, get_attr, None)
+    try:
+        return int(v) * invscale // scale  # type: ignore (some long ass bullshit error that doesnt matter)
+    except (ValueError, TypeError, OverflowError):
+        return default
+
+class LazyList(collections.abc.Sequence):
+    """Lazy immutable list from an iterable
+    Note that slices of a LazyList are lists and not LazyList"""
+
+    class IndexError(IndexError):
+        pass
+
+    def __init__(self, iterable, *, reverse=False, _cache=None):
+        self._iterable = iter(iterable)
+        self._cache = [] if _cache is None else _cache
+        self._reversed = reverse
+
+    def __iter__(self):
+        if self._reversed:
+            # We need to consume the entire iterable to iterate in reverse
+            yield from self.exhaust()
+            return
+        yield from self._cache
+        for item in self._iterable:
+            self._cache.append(item)
+            yield item
+
+    def _exhaust(self):
+        self._cache.extend(self._iterable)
+        self._iterable = []  # Discard the emptied iterable to make it pickle-able
+        return self._cache
+
+    def exhaust(self):
+        """Evaluate the entire iterable"""
+        return self._exhaust()[::-1 if self._reversed else 1]
+
+    @staticmethod
+    def _reverse_index(x):
+        return None if x is None else ~x
+
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            if self._reversed:
+                idx = slice(self._reverse_index(idx.start), self._reverse_index(idx.stop), -(idx.step or 1))
+            start, stop, step = idx.start, idx.stop, idx.step or 1
+        elif isinstance(idx, int):
+            if self._reversed:
+                idx = self._reverse_index(idx)
+            start, stop, step = idx, idx, 0
+        else:
+            raise TypeError('indices must be integers or slices')
+        if ((start or 0) < 0 or (stop or 0) < 0
+                or (start is None and step < 0)
+                or (stop is None and step > 0)):
+            # We need to consume the entire iterable to be able to slice from the end
+            # Obviously, never use this with infinite iterables
+            self._exhaust()
+            try:
+                return self._cache[idx]  # type: ignore (idc, not my code)
+            except IndexError as e:
+                raise self.IndexError(e) from e
+        n = max(start or 0, stop or 0) - len(self._cache) + 1
+        if n > 0:
+            self._cache.extend(itertools.islice(self._iterable, n))
+        try:
+            return self._cache[idx]  # type: ignore (idc, not my code)
+        except IndexError as e:
+            raise self.IndexError(e) from e
+
+    def __bool__(self):
+        try:
+            self[-1] if self._reversed else self[0]
+        except self.IndexError:
+            return False
+        return True
+
+    def __len__(self):
+        self._exhaust()
+        return len(self._cache)
+
+    def __reversed__(self):
+        return type(self)(self._iterable, reverse=not self._reversed, _cache=self._cache)
+
+    def __copy__(self):
+        return type(self)(self._iterable, reverse=self._reversed, _cache=self._cache)
+
+    def __repr__(self):
+        # repr and str should mimic a list. So we exhaust the iterable
+        return repr(self.exhaust())
+
+    def __str__(self):
+        return repr(self.exhaust())
+
+def traverse_obj(
+        obj, *paths, default=NO_DEFAULT, expected_type=None, get_all=True,
+        casesense=True, is_user_input=False, traverse_string=False):
+    is_sequence = lambda x: isinstance(x, collections.abc.Sequence) and not isinstance(x, (str, bytes))
+    casefold = lambda k: k.casefold() if isinstance(k, str) else k
+
+    if isinstance(expected_type, type):
+        type_test = lambda val: val if isinstance(val, expected_type) else None
+    else:
+        type_test = lambda val: try_call(expected_type or IDENTITY, args=(val,))
+
+    def apply_key(key, obj, is_last):
+        branching = False
+        result = None
+
+        if obj is None and traverse_string:
+            pass
+
+        elif key is None:
+            result = obj
+
+        elif isinstance(key, set):
+            assert len(key) == 1, 'Set should only be used to wrap a single item'
+            item = next(iter(key))
+            if isinstance(item, type):
+                if isinstance(obj, item):
+                    result = obj
+            else:
+                result = try_call(item, args=(obj,))
+
+        elif isinstance(key, (list, tuple)):
+            branching = True
+            result = itertools.chain.from_iterable(
+                apply_path(obj, branch, is_last)[0] for branch in key)
+
+        elif key is ...:
+            branching = True
+            if isinstance(obj, collections.abc.Mapping):
+                result = obj.values()
+            elif is_sequence(obj):
+                result = obj
+            elif isinstance(obj, re.Match):
+                result = obj.groups()
+            elif traverse_string:
+                branching = False
+                result = str(obj)
+            else:
+                result = ()
+
+        elif callable(key):
+            branching = True
+            if isinstance(obj, collections.abc.Mapping):
+                iter_obj = obj.items()
+            elif is_sequence(obj):
+                iter_obj = enumerate(obj)
+            elif isinstance(obj, re.Match):
+                iter_obj = itertools.chain(
+                    enumerate((obj.group(), *obj.groups())),
+                    obj.groupdict().items())
+            elif traverse_string:
+                branching = False
+                iter_obj = enumerate(str(obj))
+            else:
+                iter_obj = ()
+
+            result = (v for k, v in iter_obj if try_call(key, args=(k, v)))
+            if not branching:  # string traversal
+                result = ''.join(result)
+
+        elif isinstance(key, dict):
+            iter_obj = ((k, _traverse_obj(obj, v, False, is_last)) for k, v in key.items())
+            result = {
+                k: v if v is not None else default for k, v in iter_obj
+                if v is not None or default is not NO_DEFAULT
+            } or None
+
+        elif isinstance(obj, collections.abc.Mapping):
+            result = (obj.get(key) if casesense or (key in obj) else
+                      next((v for k, v in obj.items() if casefold(k) == key), None))
+
+        elif isinstance(obj, re.Match):
+            if isinstance(key, int) or casesense:
+                with contextlib.suppress(IndexError):
+                    result = obj.group(key)
+
+            elif isinstance(key, str):
+                result = next((v for k, v in obj.groupdict().items() if casefold(k) == key), None)
+
+        elif isinstance(key, (int, slice)):
+            if is_sequence(obj):
+                branching = isinstance(key, slice)
+                with contextlib.suppress(IndexError):
+                    result = obj[key]
+            elif traverse_string:
+                with contextlib.suppress(IndexError):
+                    result = str(obj)[key]
+
+        return branching, result if branching else (result,)
+
+    def lazy_last(iterable):
+        iterator = iter(iterable)
+        prev = next(iterator, NO_DEFAULT)
+        if prev is NO_DEFAULT:
+            return
+
+        for item in iterator:
+            yield False, prev
+            prev = item
+
+        yield True, prev
+
+    def apply_path(start_obj, path, test_type):
+        objs = (start_obj,)
+        has_branched = False
+
+        key = None
+        for last, key in lazy_last(variadic(path, (str, bytes, dict, set))):
+            if is_user_input and isinstance(key, str):
+                if key == ':':
+                    key = ...
+                elif ':' in key:
+                    key = slice(*map(int_or_none, key.split(':')))
+                elif int_or_none(key) is not None:
+                    key = int(key)
+
+            if not casesense and isinstance(key, str):
+                key = key.casefold()
+
+            if __debug__ and callable(key):
+                # Verify function signature
+                inspect.signature(key).bind(None, None)
+
+            new_objs = []
+            for obj in objs:
+                branching, results = apply_key(key, obj, last)
+                has_branched |= branching
+                new_objs.append(results)
+
+            objs = itertools.chain.from_iterable(new_objs)
+
+        if test_type and not isinstance(key, (dict, list, tuple)):
+            objs = map(type_test, objs)
+
+        return objs, has_branched, isinstance(key, dict)
+
+    def _traverse_obj(obj, path, allow_empty, test_type):
+        results, has_branched, is_dict = apply_path(obj, path, test_type)
+        results = LazyList(item for item in results if item not in (None, {}))
+        if get_all and has_branched:
+            if results:
+                return results.exhaust()
+            if allow_empty:
+                return [] if default is NO_DEFAULT else default
+            return None
+
+        return results[0] if results else {} if allow_empty and is_dict else None
+
+    for index, path in enumerate(paths, 1):
+        result = _traverse_obj(obj, path, index == len(paths), True)
+        if result is not None:
+            return result
+
+    return None if default is NO_DEFAULT else default
+##################################################### yt-dlp/yt-dlp/yt_dlp/utils.py <3
