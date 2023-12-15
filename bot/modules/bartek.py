1e480a54a0b7fe873626b6d7571f762ea04ab73e
szykrz15@wp.pl <szykrz15@wp.pl>
2023-12-15 12:44:28 +0100

    Minor fixes

--- bot/modules/bartek.py
+++ bot/modules/bartek.py
@@ -1,36 +1,39 @@
+import logging
 import discord
 from discord.ext import commands
 
 from ..bot import DegeneratBot
 
 import csv
-from os import popen
+import os
 from datetime import date
 
-GUILD_ID = 1121557176390012958 #Alfa romeo
-USER_ID = 410891559052247050 #Bartosz Tokarski ID
-
+PATH = "./data/polityka"
 
 class Bartek(commands.Cog):
     def __init__(self, bot: DegeneratBot):
         self.bot: DegeneratBot = bot
 
+        guild_id = os.getenv("GUILD_ID")
+        if not guild_id:
+            return logging.fatal("GUILD_ID environment variable not set!")
+
+        bartek_id = os.getenv("BARTEK_ID")
+        if not bartek_id:
+            return logging.fatal("BARTEK_ID environment variable not set!")
+
         self.bartekCount = 0
         self.blacklist = []
         self.previousDate = date.today()
 
-        files = popen("ls bot/modules/_polityka/").read().split("\n")[:-1]
-
-        for file in files:
-            with open(f"bot/modules/_polityka/{file}", "r") as f:  
+        for file in os.listdir(PATH):
+            with open(f"{PATH}/{file}", "r") as f:
                 self.blacklist.extend(list(csv.reader(f, delimiter=";"))[0][:-1])
-        
+
         self.blacklist = set(self.blacklist)
-        print(self.blacklist)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-
         print(message.content)
 
         if message.author.bot or not message.content:
@@ -39,16 +42,15 @@ class Bartek(commands.Cog):
         if message.content.startswith(self.bot.command_prefix):  # type: ignore (error: Argument of type "PrefixType[BotT@__init__]" cannot be assigned to parameter "__prefix" of type "str | tuple[str, ...]" in function "startswith"  - bruh)
             return
 
-        if message.guild.id != GUILD_ID:
+        if message.guild.id != guild_id:
             return
 
-        if message.author.id != USER_ID:
+        if message.author.id != bartek_id:
             return
-        
 
         if not set(message.content.lower().split()).intersection(self.blacklist):
             return
-        
+
         if self.previousDate != date.today():
             self.bartekCount = 0
             self.previousDate = date.today()
@@ -56,9 +58,10 @@ class Bartek(commands.Cog):
         if self.bartekCount >= 2:
             await message.delete()
             return
-            
+
         self.bartekCount += 1
         return
-    
+
+
 async def setup(bot: DegeneratBot):
     await bot.add_cog(Bartek(bot))
