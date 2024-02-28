984d554b1e94c813190004250f0ed30805d81425
drobotk <pawwwll@gmail.com>
2024-02-28 21:36:31 +0100

    Add `AllegroEmbed` cog

--- bot/bot.py
+++ bot/bot.py
@@ -25,7 +25,8 @@ class DegeneratBot(commands.Bot):
     async def setup_hook(self) -> None:
         self.session: aiohttp.ClientSession = aiohttp.ClientSession(
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+                "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
             }
         )
 
