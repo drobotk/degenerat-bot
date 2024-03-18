6950c24f6d679ad65af263d8a0a01b4f5db19e78
Ogoorass <igor.piontek@wp.pl>
2024-03-18 17:22:37 +0100

    formatted

--- bot/modules/bartek/twitterHandler.py
+++ bot/modules/bartek/twitterHandler.py
@@ -1,6 +1,7 @@
 import aiohttp
 import logging
 import json
+import re
 
 from .textHandler import TextHandler
 
@@ -13,13 +14,15 @@ class TwitterHandler:
     async def isOffending(self, url: str) -> bool:
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.get("https://publish.twitter.com/oembed?url=" + url) as resp: # returns json embed data
-                    twitter_data_json = await resp.text()
+                async with session.get(
+                    "https://publish.twitter.com/oembed?url=" + url
+                ) as resp:  # returns json embed data
+                    twitter_embed_data_json = await resp.text()
 
         except:
             self.log.error("Bad url - twitter")
 
-        twitter_data = json.loads(twitter_data_json)
+        twitter_data = json.loads(twitter_embed_data_json)
 
         # this wierd url returns a json with some embeded data
         # i hope it will work
