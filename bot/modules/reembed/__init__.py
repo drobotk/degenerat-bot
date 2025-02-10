fc9f0e51e9ba2fe210492228ca67d600cb00e88d
drobotk <pawwwll@gmail.com>
2025-02-10 16:38:15 +0100

    ReEmbed: disable broken AllegroEmbed

--- bot/modules/reembed/__init__.py
+++ bot/modules/reembed/__init__.py
@@ -1,14 +1,15 @@
 import asyncio
 
 from ...bot import DegeneratBot
-from .allegro import AllegroReEmbed
+
+# from .allegro import AllegroReEmbed
 from .videos import VideosReEmbed
 from .yt_posts import YoutubePostsReEmbed
 
 
 async def setup(bot: DegeneratBot):
     await asyncio.gather(
-        bot.add_cog(AllegroReEmbed(bot)),
+        # bot.add_cog(AllegroReEmbed(bot)),
         bot.add_cog(VideosReEmbed(bot)),
         bot.add_cog(YoutubePostsReEmbed(bot)),
     )
