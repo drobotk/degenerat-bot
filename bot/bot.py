d09cfaa92aea007fc451f26148df84ce159571d8
Dawid Krajcarz <80264606+drobotk@users.noreply.github.com>
2026-01-06 15:32:13 +0100

    fix(Music - YouTube): attempt to fix yt search

--- bot/bot.py
+++ bot/bot.py
@@ -32,6 +32,7 @@ class DegeneratBot(commands.Bot):
             headers={
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0",
                 "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
+                "Accept-Encoding": "gzip, deflate"
             }
         )
 
